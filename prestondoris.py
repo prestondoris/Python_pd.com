d3969cba7cae2df07851421ad949563a590f65a5
Preston Doris <prestondoris@gmail.com>
2018-06-28 21:40:31 -0700

    Create first render template for resume page.

--- prestondoris.py
+++ prestondoris.py
@@ -1,12 +1,13 @@
-from flask import Flask
+from flask import Flask, render_template, url_for, request
+from flask import redirect, flash, jsonify, make_response
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def resume():
     '''
     Main route for my resume page.
     '''
-    return "Hello World!!!!!!"
+    return render_template('resume.html')
 
 
 
